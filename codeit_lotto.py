ad33149b124d3e713e10fdabbb6e3888b28bcd5f
minjae <32402820+kkrlee@users.noreply.github.com>
2019-02-13 17:58:53 -0500

    second upload: fixed second def

--- codeit_lotto.py
+++ codeit_lotto.py
@@ -1,11 +1,9 @@
 from random import randint
 
-num_list = []
-bonus_num_list = []
-
 def generate_numbers():
+    num_list = []
     while len(num_list) != 6:
-        num = randint(1, 46)
+        num = randint(1, 45)
         if num in num_list:
             continue
         else:
@@ -13,17 +11,19 @@ def generate_numbers():
     num_list.sort()
     return num_list
 
-list1 = generate_numbers()
-list2 = draw_winning_numbers()
-
-print(list1)
-print(list2)
 
 def draw_winning_numbers():
+    bonus_num_list = []
     bonus_num_list = generate_numbers()
-    bonus_num = randint(1, 46)
-    bonus_num_list.append(bonus_num)
+    bonus_num = randint(1, 45)
+    if bonus_num not in bonus_num_list:
+        bonus_num_list.append(bonus_num)
     return bonus_num_list
+
+list1 = generate_numbers()
+list2 = draw_winning_numbers()
+print(list1)
+print(list2)
 
 def count_matching_numbers(list1, list2):
     count = 0
@@ -35,25 +35,16 @@ def count_matching_numbers(list1, list2):
 def check(numbers, winning_numbers):
     same_number = count_matching_numbers(numbers, winning_numbers)
     if same_number == 6:
+        return 1000000000
+    elif same_number == 5:
         if winning_numbers[6] in numbers:
             return 50000000
         else:
-            return 100000000
-    elif same_number == 5:
-        if winning_numbers[6] in numbers:
-            return 50000
-        else:
             return 1000000
     elif same_number == 4:
-        if winning_numbers[6] in numbers:
-            return 5000
-        else:
-            return 50000
+        return 50000
     elif same_number == 3:
-        if winning_numbers[6] in numbers:
-            return 0
-        else:
-            return 5000
+        return 5000
     else:
         return 0
 
